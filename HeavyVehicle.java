92530feae146f92de457b16dc7f3f89a1c2d2957
Prime38 <shoumikprime@gmail.com>
2020-07-05 00:30:46 +0600

    show vehicle list with unique id and cexpected visitor count

--- HeavyVehicle.java
+++ HeavyVehicle.java
@@ -9,7 +9,7 @@ public class HeavyVehicle extends Vehicle{
 		this.weight = weight;
 	}
 	public HeavyVehicle(String model,int enginePower, int tireSize,int weight) {
-		super(VehicleType.HEAVY, model, EngineType.DIESEl, enginePower, tireSize);
+		super(VehicleType.HEAVY, model, EngineType.DIESEL, enginePower, tireSize);
 		this.weight=weight;
 		
 	}
